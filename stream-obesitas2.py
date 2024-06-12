feb63e364d9d3cbf09ddd472ea49ec3a51588b4b
Dede Ratna Sari <161820574+derasa16@users.noreply.github.com>
2024-06-12 19:35:55 +0700

    Update stream-obesitas2.py

--- stream-obesitas2.py
+++ stream-obesitas2.py
@@ -16,7 +16,7 @@ try:
     scaler = pickle.load(open('StandardScaler.pkl', 'rb'))
     
     # Membaca label encoder
-    with open('Encoder.pkl', 'rb') as file:
+    with open('EncoderS (1).pkl', 'rb') as file:
         label_encoders = pickle.load(file)
     
     st.write("Model, scaler, and label encoder loaded successfully!")
